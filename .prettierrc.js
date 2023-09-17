97aab64dafae92f99ca46b03a50ced76e15d5ce9
Misael <maq3009@hotmail.com>
2023-09-17 11:21:14 -0400

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  semi: false
+};
